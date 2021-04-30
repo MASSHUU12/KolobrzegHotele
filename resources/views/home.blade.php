7c6a3bb7b8765472c83b967c0566f9e19a120ad4
Jakub <jakub@slaweks-macbook-air.home>
2021-04-30 15:52:08 +0200

    first'

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('master')
+
+@section('title', 'Homepage')
+
+@section('content')
+
+<h1>content</h1>
+
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
