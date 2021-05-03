e660c3cc13cf311f5d91313710ec39be98f91bda
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2021-05-03 11:01:30 +0200

    there is a bit of it
    wrzucam zanim stracę Internet bo co chwilę go wywala, są:
    - 2 flagi
    - poprawione teksty
    - CDN fontawesome
    - jedna ikona bo jak daję więcej to wszystko dziwnie się rozjeżdża, potem się poprawi

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>@yield('title')</title>
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        <header>
-            <div class="header-logo">KołobrzegHotele</div>
-            <div class="header-links">
-                <ul>
-                    <li>one</li>
-                    <li>one</li>
-                    <li><img src="{{ asset('img/polish.png') }}" class="language-flag" alt="language"></li>
-                </ul>
-            </div>
-        </header>
-        @yield('content')
-        <footer>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- Font Awesome -->
+    <script src="https://kit.fontawesome.com/2db9ab2272.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <div class="header-logo">KołobrzegHotele</div>
+        <div class="header-links">
+            <ul>
+                <li><img src="{{ asset('img/ico_pl.png') }}" class="language-flag" alt="language"></li>
+                <li><img src="{{ asset('img/ico_ge.png') }}" class="language-flag" alt="language"></li>
+                <li><img src="{{ asset('img/ico_ge.png') }}" class="language-flag" alt="language"></li>
+            </ul>
+        </div>
+    </header>
+    @yield('content')
+    <footer>
         footer
-        </footer>
-    </body>
+    </footer>
+</body>
+
 </html>
