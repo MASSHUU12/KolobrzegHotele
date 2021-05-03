ea673a3efc850e5ab3278146f3f6d493b0a730f5
Jakub <jakub@slaweks-macbook-air.home>
2021-05-03 17:37:17 +0200

    api works

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,16 +1,14 @@
 @extends('master')
 
-@section('title', 'Homepage')
+@section('title', 'szukaj noclegu')
 
 @section('content')
 
-<section class="home-search">
+<section class="home-search search-page-search">
     <div class="search-container-image">
         <img src="{{ asset('img/port-low-res.png') }}" data-src="{{ asset('img/port.png') }}" id="main-image">
     </div>
-    <h1 class="main-text">Twój wypoczynek <br> w Kołobrzegu</h1>
     <div class="search-container">
-        <p>Zaznacz co jest dla ciebie najważniejsze, a my znajdziemy twój idealny nocleg.</p>
         <form action="/search" method="get">
             <div class="searchbar">
                 <div class="searchbar-filter">
@@ -120,36 +118,28 @@
         </form>
     </div>
 </section>
-<div class="features-container">
-    <div class="feature-both">
-        <div class="feature-image">
-            <img class="feature-img" src="{{ asset('img/ico_main1.png') }}" />
-        </div>
-        <div class="feature-text">
-            <h1>Wyszukiwarka od <br> turystów dla turystów</h1>
-            <p>Wyszukiwanie dogodnego miejsca nigdy nie było tak proste! Nowoczesny, prosty i elegancki design sprawia, że znalezienie
-                idealnej oferty to kwestia kilku minut.</p>
-        </div>
+
+<section class="search-results-container">
+    <div class="search-results-container-inner">
+        asdf
+        <table border="1">
+            <th>
+                <td>ulica</td>
+                <td>1</td>
+                <td>1</td>
+            </th>
+            @foreach ($results as $item)
+                <tr>
+                    <td>{{$item['nazwa_obiektu']}}</td>
+                    <td>{{$item['ulica']}}</td>
+                    <td>1</td>
+                    <td>1</td>
+                </tr>
+            @endforeach
+        </table>
     </div>
-    <div class="feature-both feature-right">
-        <div class="feature-image">
-            <img class="feature-img" src="{{ asset('img/ico_main2.png') }}" />
-        </div>
-        <div class="feature-text">
-            <h1>Znajdź swoje <br> wymarzone miejsce</h1>
-            <p>Wybierz to, co dla ciebie jest najważniejsze, a my dopasujemy najlepsze miejsca do twoich preferencji, abyś mógł spędzić czas tak jak to zaplanowałeś.</p>
-        </div>
-    </div>
-    <div class="feature-both">
-        <div class="feature-image">
-            <img class="feature-img" src="{{ asset('img/ico_main3.png') }}" />
-        </div>
-        <div class="feature-text">
-            <h1>Jak to działa?</h1>
-            <p>Nasz innowacyjny system wyszukiwania ofert sam dopasuje najlepszy nocleg do twoich preferencji. Wszystko co musisz zrobić, to wybrać jak ważne są dla ciebie podane kategorie.</p>
-        </div>
-    </div>
-</div>
+</section>
+
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js" integrity="sha512-cdV6j5t5o24hkSciVrb8Ki6FveC2SgwGfLE31+ZQRHAeSRxYhAQskLkq3dLm8ZcWe1N3vBOEYmmbhzf7NTtFFQ==" crossorigin="anonymous"></script>
 <script src="{{ asset('js/searchbarDropdown.js') }}"></script>
