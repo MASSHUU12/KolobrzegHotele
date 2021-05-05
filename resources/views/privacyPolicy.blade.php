520487662849b9e3297071ca72fdb507b8f35aef
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2021-05-05 15:21:33 +0200

    subpages

---
+++ resources/views/privacyPolicy.blade.php
@@ -0,0 +1,27 @@
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Polityka Prywatności</title>
+    <link rel="stylesheet" type="text/css" href="./style.css" />
+</head>
+<body>
+    <h1>Polityka Prywatności</h1>
+    <p>Nasza Polityka Prywatności informuje jak przetwarzamy dane osobowe naszych Użytkowników.</p>
+    <h1>W jakim celu przetwarzamy Twoje dane?</h1>
+    <p>Bez tego nie bylibyśmy w stanie świadczyć usługi na odpowiednim poziomie i nie bylibyśmy w stanie pomóc użytkownikowi. Zbieramy i przetwarzamy dane użytkownika tylko wtedy, gdy skontaktuje się z działem pomocy technicznej.</p>
+    <h1>Jakie dane przetwarzamy?</h1>
+    <p>Przetwarzamy Twoje dane zawierające adres email oraz wszystkie inne informacje, jakie nam podasz, które będą niezbędne, aby rozwiązać Twój problem (np. informacje o urządzeniu, przeglądarce). Zakres przetwarzanych konkretnych danych jest różny w zależności od problemu.</p>
+    <h1>Jak długo przetwarzamy dane?</h1>
+    <p>Twoje dane będą przetwarzane do czasu rozwiązania problemu, zrezygnowania z naszej usługi lub cofnięcia zgody na przetwarzanie tych danych (udzielasz zgodę na przetwarzanie danych kontaktując się z działem pomocy, natomiast odebrać ją możesz wysyłając do nas wiadomość z taką informacją).</p>
+    <h1>Jakie prawa przysługują naszym Użytkownikom jako podmiotom danych osobowych?</h1>
+    <p>Zgodnie z przepisami Ogólnego Rozporządzenia dotyczącego Ochrony Danych Osobowych (rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z 27.04.2016r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE - (RODO)), przysługują Ci następujące prawa w związku z przetwarzaniem Twoich danych osobowych przez nas: </p>
+    <p>- prawo dostępu do Twoich danych, w tym uzyskania kopii danych,<br />- prawo żadania sprostowania danych, <br />- prawo do usunięcia danych (prawo do bycia zapomnianym),<br />- prawo wniesienia skargi do organu nadzorczego ochrony danych osobowych, jakim jest w Polsce Prezes Urzędu Ochrony Danych Osobowych,<br />- prawo do ograniczenia przetwarzania danych,<br />- prawo sprzeciwu (sprzeciw wobec przetwarzania, sprzeciw wobec marketingu bezpośredniego, w tym profilowania, sprzeciw uzasadniony wobec przetwarzania Twoich danych w uzasadnionym interesie Administratora).</p>
+    <p>Jeżeli Twoje dane są przetwarzane na podstawie zgody:</p>
+    <p>- masz prawo do wycofania zgody. Wycofanie zgody nie ma wpływu na zgodność z prawem przetwarzania, którego dokonano na podstawie zgody przed jej wycofaniem. Jeżeli Twoje dane są przetwarzane na podstawie zgody lub w celu realizacji umowy (niezbędne w celu świadczenia usługi):<br />- masz prawo do przenoszenia danych osobowych.</p>
+    <p>W celu realizacji powyższych praw należy skontaktować się z nami przez dział pomocy technicznej.</p>
+    <h1>Komu odostępniamy dane?</h1>
+    <p>Twoje dane nie są nikomu udostępniane, przetwarzamy je sami i wykorzystujemy w taki sposób, aby pomagać użytkownikowi.</p>
+</body>
+</html>
