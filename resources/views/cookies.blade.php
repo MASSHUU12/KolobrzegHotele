f3ae1c9dc327c4a94cb2a91e9bf002c1e6aff45e
Jakub <jakub@Slaweks-MacBook-Air.local>
2021-05-05 18:52:51 +0200

    pages

--- resources/views/cookies.blade.php
+++ resources/views/cookies.blade.php
@@ -4,6 +4,38 @@
 
 @section('content')
 
-cookies
+<section class="info-page">
+    <div class="info-page-inner">
+        <div class="info-title">
+            <div>
+                <h1>jestesmy owcami</h1>
+                <p>to prawda</p>
+            </div>
+        </div>
+        <div class="info-top">
+            <div class="info-main-image">
+                <img src="{{ asset('img/tempmap.png') }}" alt="">
+            </div>
+            <div>
+                <h2>owce sa fajnie</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ipsam adipisci accusamus quia voluptatem totam harum nulla nam voluptatum ipsa accusantium non, neque officia repellat atque cupiditate similique deserunt fuga.</p>
+            </div>
+        </div>
+        <div class="info-long">
+            <article>
+                <h3>co jedza owce</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+            </article>
+            <article>
+                <h3>co jedza owce</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+            </article>
+            <article>
+                <h3>co jedza owce</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+            </article>
+        </div>
+    </div>
+</section>
 
 @endsection
