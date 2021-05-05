abcaefff28089c2f63ec2f9030b9a923f34e136d
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2021-05-05 20:36:09 +0200

    subpages and footer fix

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -8,31 +8,56 @@
     <div class="info-page-inner">
         <div class="info-title">
             <div>
-                <h1>jestesmy owcami</h1>
-                <p>to prawda</p>
+                <h1>Polityka prywatności</h1>
+                <p>Niniejsza Polityka Prywatnośći informuje jak przetwarzamy
+                    dane osobowe naszych Użytkowników.</p>
             </div>
         </div>
         <div class="info-top">
             <div class="info-main-image">
-                <img src="{{ asset('img/tempmap.png') }}" alt="">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="30em" height="30em" style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
+                    <path d="M12 3.19l7 3.11V11c0 4.52-2.98 8.69-7 9.93c-4.02-1.24-7-5.41-7-9.93V6.3l7-3.11M12 1L3 5v6c0 5.55 3.84 10.74 9 12c5.16-1.26 9-6.45 9-12V5l-9-4zm-1 6h2v2h-2V7zm0 4h2v6h-2v-6z" fill="#234B63" />
+                </svg>
             </div>
             <div>
-                <h2>owce sa fajnie</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo ipsam adipisci accusamus quia voluptatem totam harum nulla nam voluptatum ipsa accusantium non, neque officia repellat atque cupiditate similique deserunt fuga.</p>
+                <h2>W jakim celu przetwarzamy Twoje dane?</h2>
+                <p>Bez tego nie bylibyśmy w stanie świadczyć usługi na odpowiednim poziomie i nie bylibyśmy w stanie pomóc użytkownikowi. Zbieramy i przetwarzamy dane użytkownika tylko wtedy, gdy skontaktuje się z działem pomocy technicznej.</p>
             </div>
         </div>
         <div class="info-long">
             <article>
-                <h3>co jedza owce</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+                <h3>Jakie dane przetwarzamy?</h3>
+                <p>Przetwarzamy Twoje dane zawierające adres email oraz wszystkie inne informacje, jakie nam podasz, które będą niezbędne, aby rozwiązać Twój problem (np. informacje o urządzeniu, przeglądarce). Zakres przetwarzanych konkretnych danych jest różny w zależności od problemu.</p>
             </article>
             <article>
-                <h3>co jedza owce</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+                <h3>Jak długo przetwarzamy dane?</h3>
+                <p>Twoje dane będą przetwarzane do czasu rozwiązania problemu, zrezygnowania z naszej usługi lub cofnięcia zgody na przetwarzanie tych danych (udzielasz zgodę na przetwarzanie danych kontaktując się z działem pomocy, natomiast odebrać ją możesz wysyłając do nas wiadomość z taką informacją).</p>
             </article>
             <article>
-                <h3>co jedza owce</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, tenetur?</p>
+                <h3>Jakie prawa przysługują naszym Użytkownikom jako podmiotom danych osobowych?</h3>
+                <p>Zgodnie z przepisami Ogólnego Rozporządzenia dotyczącego Ochrony Danych Osobowych (rozporządzenie Parlamentu Europejskiego i Rady (UE) 2016/679 z 27.04.2016r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych oraz uchylenia dyrektywy 95/46/WE - (RODO)), przysługują Ci następujące prawa w związku z przetwarzaniem Twoich danych osobowych przez nas:
+                </p>
+                <ul>
+                    <li>prawo dostępu do Twoich danych, w tym uzyskania kopii danych</li>
+                    <li>prawo żadania sprostowania danych</li>
+                    <li>prawo do usunięcia danych (prawo do bycia zapomnianym)</li>
+                    <li>prawo wniesienia skargi do organu nadzorczego ochrony danych osobowych, jakim jest w Polsce Prezes Urzędu Ochrony Danych Osobowych</li>
+                    <li>prawo do ograniczenia przetwarzania danych</li>
+                    <li>prawo sprzeciwu (sprzeciw wobec przetwarzania, sprzeciw wobec marketingu bezpośredniego, w tym profilowania, sprzeciw uzasadniony wobec przetwarzania Twoich danych w uzasadnionym interesie Administratora)</li>
+                </ul>
+            </article>
+            <article>
+                <p>Jeżeli Twoje dane są przetwarzane na podstawie zgody:
+                </p>
+                <ul>
+                    <li>masz prawo do wycofania zgody. Wycofanie zgody nie ma wpływu na zgodność z prawem przetwarzania, którego dokonano na podstawie zgody przed jej wycofaniem. Jeżeli Twoje dane są przetwarzane na podstawie zgody lub w celu realizacji umowy (niezbędne w celu świadczenia usługi) masz prawo do przenoszenia danych osobowych.
+                    </li>
+                </ul>
+                <p>W celu realizacji powyższych praw należy skontaktować się z nami przez dział pomocy technicznej.</p>
+            </article>
+            <article>
+                <h3>Komu udostępniamy dane?</h3>
+                <p>Twoje dane nie są nikomu udostępniane, przetwarzamy je sami i wykorzystujemy w taki sposób, aby pomagać użytkownikowi.</p>
             </article>
         </div>
     </div>
